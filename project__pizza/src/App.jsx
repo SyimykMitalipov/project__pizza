0df9ed19b792cadbfd2db31454fadfdfc318e02b
Syimyk <syimyk310703@gmail.com>
2022-03-08 14:37:32 +0600

    I have made 5 part of pizza project

--- project__pizza/src/App.jsx
+++ project__pizza/src/App.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components";
 import { Home ,  Cart} from "./pages";
+import axios from "axios";
 // import SortPopup from "./components/SortPopup";
 
 
 
-
 const App = () => {
-  const clickKnopka = () => {
-        alert('Syimyk Mitalipov')
-  }
+  const [pizzas , setPizzas] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas)
+    })
+
+  },[])
     return (
         <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-            <Route path="/" element={<Home />} exact />
+            <Route path="/" element={ <Home items={pizzas} />} exact />
             <Route path="/cart" element={<Cart />} exact />
         </Routes>
         {/* <Cart /> */}
--- project__pizza/src/components/index.js
+++ project__pizza/src/components/index.js
@@ -2,6 +2,6 @@ import Button from "./Button";
 import Categories from "./Categories";
 import Header from "./Header";
 import SortPopup from "./SortPopup";
+import Pizzablock from "./PizzaBlock";
 
-
-export { Button , Categories , Header , SortPopup}
+export { Button , Categories , Header , SortPopup, Pizzablock}
